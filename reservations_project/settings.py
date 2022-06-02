053f3e8f1ce7fa2db43ceacd99c6b01079ceca80
bezalel001 <mickeybezalel@gmail.com>
2022-06-02 12:19:26 +0100

    chore: Created guests app and added project-level templates

--- reservations_project/settings.py
+++ reservations_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Local
     'customusers.apps.CustomusersConfig',
+    'guests.apps.GuestsConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'reservations_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
