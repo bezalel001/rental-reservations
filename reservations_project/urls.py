524b5f64ad7ca310674582aeacfe1691c933dc27
bezalel001 <mickeybezalel@gmail.com>
2022-06-06 05:32:26 +0100

    security: Changed default admin url

--- reservations_project/urls.py
+++ reservations_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secret-dashboard/', admin.site.urls),
     path('', include('guests.urls'))
 ]
 
