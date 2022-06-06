056f3f3972b5032f767dd61a7e26ef09aa795856
bezalel001 <mickeybezalel@gmail.com>
2022-06-06 05:15:49 +0100

    security: Added guard against XSS attacks in production

--- reservations_project/settings.py
+++ reservations_project/settings.py
@@ -157,4 +157,6 @@ hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 
 
-
+#production
+if ENVIRONMENT == 'production':
+    SECURE_BROWSER_XSS_FILTER = True
